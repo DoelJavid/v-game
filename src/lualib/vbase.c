f1440d2a3e9b0b5359ca2fc8842eb36b2342d3ff
DoelJavid <jdryerson@att.net>
2025-08-19 15:20:44 -0400

    feat: Extended Lua iterators

--- src/lualib/vbase.c
+++ src/lualib/vbase.c
@@ -8,6 +8,78 @@
 
 #include "vbase.h"
 
+static int luavbase_ipairs_next(lua_State* L) {
+  int idx = luaL_checkint(L, 2) + 1;
+
+  if (lua_istable(L, 1)) {
+    lua_pushinteger(L, idx);
+    lua_rawgeti(L, 1, idx);
+    return (lua_isnil(L, -1)) ? 0 : 2;
+  } else if (lua_isstring(L, 1)) {
+    size_t str_length;
+    const char* str = lua_tolstring(L, 1, &str_length);
+
+    if (idx <= str_length) {
+      lua_pushinteger(L, idx);
+      lua_pushlstring(L, &str[idx - 1], 1);
+      return 2;
+    }
+    return 0;
+  }
+  return luaL_error(L, "Expected table or string!");
+}
+
+static int luavbase_ipairs(lua_State* L) {
+  int type = lua_type(L, 1);
+  if (type == LUA_TTABLE || type == LUA_TSTRING) {
+    lua_pushcfunction(L, luavbase_ipairs_next);
+    lua_pushvalue(L, 1);
+    lua_pushinteger(L, 0);
+    return 3;
+  }
+  return luaL_error(L, "Expected table or string!");
+}
+
+static int luavbase_next(lua_State* L) {
+  if (lua_istable(L, 1)) {
+    lua_settop(L, 2);
+    if (lua_next(L, 1))
+      return 2;
+
+    lua_pushnil(L);
+    return 1;
+  } else if (lua_isstring(L, 1)) {
+    size_t str_length;
+    const char* str = lua_tolstring(L, 1, &str_length);
+    int idx = lua_tointeger(L, 2) + 1;
+
+    if (idx <= str_length) {
+      lua_pushinteger(L, idx);
+      lua_pushlstring(L, &str[idx - 1], 1);
+      return 2;
+    }
+    return 0;
+  }
+  return luaL_error(L, "Expected table or string!");
+}
+
+static int luavbase_pairs(lua_State* L) {
+  int type = lua_type(L, 1);
+  if (type == LUA_TTABLE || type == LUA_TSTRING) {
+    lua_pushcfunction(L, luavbase_next);
+    lua_pushvalue(L, 1);
+    lua_pushnil(L);
+    return 3;
+  }
+  return luaL_error(L, "Expected table or string!");
+}
+
+static int luavbase_rawtype(lua_State* L) {
+  luaL_checkany(L, 1);
+  lua_pushstring(L, lua_typename(L, 1));
+  return 1;
+}
+
 /**
   Returns the type of the given value in the form of a string. Unlike the
   traditional `type()` function, this function supports a special case for
@@ -45,8 +117,12 @@ static int luavbase_sleep(lua_State* L) {
 void luaopen_vbase(lua_State* L) {
   // clang-format off
   static const luaL_Reg luavbase_lib[] = {
+    {"ipairs", luavbase_ipairs},
+    {"next", luavbase_next},
+    {"pairs", luavbase_pairs},
     {"sleep", luavbase_sleep},
-    {"type", luavbase_type}, {NULL, NULL}
+    {"type", luavbase_type},
+    {NULL, NULL}
   };
 
   static const char* unsafe_base_globals[] = {
